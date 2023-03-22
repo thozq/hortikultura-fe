321163bfcb21822ed8a9153cb0485b60003b5594
thozq <qqquintho@apps.ipb.ac.id>
2023-03-22 16:57:01 +0700

    filter

--- src/containers/Dinas/BerandaDinas.js
+++ src/containers/Dinas/BerandaDinas.js
@@ -1,19 +1,49 @@
 import { TuneRounded } from '@mui/icons-material';
-import { Box, Stack, Typography,Grid } from '@mui/material';
+import { Box, Stack, Typography,Grid, Drawer } from '@mui/material';
 import BaseButton from 'components/Base/BaseButton';
 import BaseCardBlanko from 'components/Base/BaseCardBlanko';
-import { CabaiEnum } from 'utils/constants';
-import TheProfileHeader from 'components/Base/TheProfileHeader';
+import React from 'react';
+// import { CabaiEnum } from 'utils/constants';
+// import TheProfileHeader from 'components/Base/TheProfileHeader';
+
 function BerandaDinas() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <TheProfileHeader />
+
+       <Drawer 
+         PaperProps={{
+          sx: { width: "444px",
+          mx: 'auto'
+         }
+        }}
+        anchor="bottom"
+        open={open}
+        onClose={handleDrawerClose}
+        onOpen={handleDrawerOpen}
+        >
+        <Box>
+         <Typography variant="h5" align="center">
+          Harga Rata-Rata
+         </Typography>
+         <Typography variant="h5" mb={1} align="center">
+          Kec.Dramaga, Kab. Bogor, Jawa Barat
+         </Typography>
+        </Box>
+        </Drawer>
       <Stack gap={2} p={2} mb="56px">
         <Stack direction="row" spacing={7} mb={3}>
-          <Typography variant="h4">
-           Statistik Deskriptif Tanaman
-          </Typography>
-          <BaseButton shape="filter" link="" size="small" variant="outlined">
+          <Typography variant="h4"> Statistik Deskriptif Tanaman </Typography>
+          <BaseButton shape="filter" link="" size="small" variant="outlined" onClick={handleDrawerOpen}>
             <TuneRounded />
             <Typography variant="subtitle1">Filter</Typography>
           </BaseButton>
