f20c80e0570ef2cea9f884e93410f949d2e5dd94
thozq <qqquintho@apps.ipb.ac.id>
2023-04-11 11:08:29 +0700

    commit

--- src/redux/slices/dinas.js
+++ src/redux/slices/dinas.js
@@ -1,107 +1,33 @@
 /* eslint-disable */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { setMessage } from './message';
-import AuthService from '../../services/auth.service';
-import { persistor } from 'redux/store';
 import { PURGE } from 'redux-persist';
-import SupervisiService from 'services/supervisi.service';
+import dinasService from 'services/dinas.service';
 
-const user = JSON.parse(localStorage.getItem('user'));
-// const childUser = JSON.parse(localStorage.getItem('child-user'));
-
-export const signup = createAsyncThunk('auth/signup', async (data, thunkAPI) => {
-  try {
-    const response = await AuthService.signup(data);
-    thunkAPI.dispatch(setMessage(response));
-    return { user: response.data.user };
-  } catch (error) {
-    const response = error.response;
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    thunkAPI.dispatch(setMessage(response));
-    return thunkAPI.rejectWithValue();
-  }
+export const filterStatistik = createAsyncThunk('dinas', async (data, thunkAPI) => {
+  const response = await dinasService.filterStatisik(data);
+  return response.data;
 });
 
-const initialState = { isLoggedIn: user ? true : false, user: null, parentUser: null, status: '' };
-const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    changeUser: (state) => {
-      localStorage.removeItem('child-user');
-      localStorage.removeItem('child-token');
-      return { isLoggedIn: true, user: state.parentUser, parentUser: state.parentUser };
-    }
-  },
+const initialState = {
+  statistik: [], status: null, detail: {}
+};
+
+const dinasSlice = createSlice({
+  name: 'dinas',
+  initialState: initialState,
   extraReducers: (builder) => {
     builder.addCase(PURGE, () => initialState);
-    builder.addCase(signup.pending, (state) => {
+    builder.addCase(filterStatistik.pending, (state) => {
       state.status = 'loading';
     });
-    builder.addCase(signup.fulfilled, (state, action) => {
-      state.isLoggedIn = true;
-      state.user = action.payload.user;
+    builder.addCase(filterStatistik.fulfilled, (state, action) => {
       state.status = 'success';
+      state.riwayat = action.payload.data;
     });
-    builder.addCase(signup.rejected, (state) => {
-      state.isLoggedIn = false;
-      state.status = 'failed';
-    });
-    builder.addCase(signin.pending, (state, action) => {
-      state.status = 'loading';
-    });
-    builder.addCase(signin.fulfilled, (state, action) => {
-      state.isLoggedIn = true;
-      state.user = action.payload.user;
-      state.parentUser = action.payload.user;
-      state.status = 'success';
-    });
-    builder.addCase(signin.rejected, (state) => {
-      state.isLoggedIn = false;
-      state.user = null;
-      state.status = 'failed';
-    });
-    builder.addCase(logout.fulfilled, (state) => {
-      state.isLoggedIn = false;
-      state.user = null;
-      state.parentUser = null;
-    });
-    builder.addCase(relog.pending, (state, action) => {
-      state.status = 'loading';
-    });
-    builder.addCase(relog.fulfilled, (state, action) => {
-      state.isLoggedIn = true;
-      state.user = action.payload.user;
-      state.status = 'success';
-    });
-    builder.addCase(relog.rejected, (state) => {
-      state.status = 'failed';
-    });
-    builder.addCase(relogById.pending, (state, action) => {
-      state.status = 'loading';
-    });
-    builder.addCase(relogById.fulfilled, (state, action) => {
-      state.user = action.payload.user;
-      state.status = 'success';
-    });
-    builder.addCase(relogById.rejected, (state) => {
-      state.status = 'failed';
-    });
-    builder.addCase(addSupervisi.pending, (state, action) => {
-      state.status = 'loading';
-    });
-    builder.addCase(addSupervisi.fulfilled, (state, action) => {
-      state.user = action.payload.user;
-      state.status = 'success';
-    });
-    builder.addCase(addSupervisi.rejected, (state) => {
+    builder.addCase(filterStatistik.rejected, (state) => {
       state.status = 'failed';
     });
   }
 });
-const { reducer, actions } = authSlice;
-export const { changeUser } = actions;
-export default reducer;
+
+export default dinasSlice.reducer;
