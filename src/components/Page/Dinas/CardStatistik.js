45927bca681fe74d424467640cd3b43fba0b0af4
thozq <qqquintho@apps.ipb.ac.id>
2023-05-25 16:03:23 +0700

    stat

--- src/components/Page/Dinas/CardStatistik.js
+++ src/components/Page/Dinas/CardStatistik.js
@@ -65,7 +65,7 @@ const CardStatistik = (props) => {
             alignItems: 'center',
             display: 'flex',
             maxWidth: 50,
-            paddingRight: '20px'
+            paddingRight: '30px'
           }}>
           {arrow}
           <Typography variant="subtitle2" style={{ color: '#424242' }}>
